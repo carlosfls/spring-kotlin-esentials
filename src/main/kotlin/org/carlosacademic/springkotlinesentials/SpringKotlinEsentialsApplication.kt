05d6d11ff20b9bea061f0ff01bc64440cbd66a9d
Carlos <carlosfls54@gmail.com>
2025-07-10 13:13:19 -0300

    Initial commit

---
+++ src/main/kotlin/org/carlosacademic/springkotlinesentials/SpringKotlinEsentialsApplication.kt
@@ -0,0 +1,11 @@
+package org.carlosacademic.springkotlinesentials
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinEsentialsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinEsentialsApplication>(*args)
+}
---
+++ src/test/kotlin/org/carlosacademic/springkotlinesentials/SpringKotlinEsentialsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.carlosacademic.springkotlinesentials
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinEsentialsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
