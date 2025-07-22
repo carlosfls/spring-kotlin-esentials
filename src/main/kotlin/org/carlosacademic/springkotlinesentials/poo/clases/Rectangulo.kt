58ebee462ebc9960260609cc849e323f7fa979bb
Carlos <carlosfls54@gmail.com>
2025-07-22 11:11:41 -0300

    Classes pt1

---
+++ src/main/kotlin/org/carlosacademic/springkotlinesentials/poo/clases/Rectangulo.kt
@@ -0,0 +1,9 @@
+package org.carlosacademic.springkotlinesentials.poo.clases
+
+class Rectangulo {
+
+    var ancho: Int = 0
+    var alto: Int = 0
+
+    fun area() = ancho * alto
+}
---
+++ src/main/kotlin/org/carlosacademic/springkotlinesentials/poo/clases/main.kt
@@ -0,0 +1,10 @@
+package org.carlosacademic.springkotlinesentials.poo.clases
+
+fun main() {
+    var rectangulo = Rectangulo()
+
+    rectangulo.ancho = 10
+    rectangulo.alto = 20
+
+    println(rectangulo.area())
+}
